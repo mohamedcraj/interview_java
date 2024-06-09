67637845f60f7ca0e9ade10da79ba14af66621f2
mohamedcraj <51104724+mohamedcraj@users.noreply.github.com>
2024-06-09 16:17:25 +0800

    initial commit

---
+++ src/test/java/com/interview/AppTest.java
@@ -0,0 +1,20 @@
+package com.interview;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+@Slf4j
+public class AppTest 
+{
+//    @Test
+//    public void shouldAnswerWithTrue()
+//    {
+//        int i,j=0;
+//
+//        assertTrue( true );
+//    }
+}
