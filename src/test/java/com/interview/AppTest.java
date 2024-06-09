61456a766325608a4fcbf8347b6f296093bdba33
mohamedcraj <51104724+mohamedcraj@users.noreply.github.com>
2024-06-09 17:41:52 +0800

    corrected java compiler issues

--- src/test/java/com/interview/AppTest.java
+++ src/test/java/com/interview/AppTest.java
@@ -1,20 +1,17 @@
 package com.interview;
 
-import static org.junit.Assert.assertTrue;
-
-import org.junit.Test;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 /**
  * Unit test for simple App.
  */
 @Slf4j
-public class AppTest 
-{
-//    @Test
-//    public void shouldAnswerWithTrue()
-//    {
-//        int i,j=0;
-//
-//        assertTrue( true );
-//    }
+public class AppTest {
+    @Test
+    public void shouldAnswerWithTrue() {
+        int i = 0, j = 0;
+        Assertions.assertEquals(i, j);
+    }
 }
