10d3ab5cb691adcfeea8282acf4b8c59d441ec27
mohamedcraj <51104724+mohamedcraj@users.noreply.github.com>
2024-06-12 14:05:18 +0800

    Updated part 1 - implementation

--- src/main/java/com/interview/TicTacToe.java
+++ src/main/java/com/interview/TicTacToe.java
@@ -1,45 +1,71 @@
 package com.interview;
 
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+@Slf4j
 public class TicTacToe {
     private final static int maxRoundPerGame = 9;
 
     public static void main(String[] args) {
+        String fileName = "src/main/resources/completed_games.txt";
 
-        String gameInput = "XOX=OX=O=";
-        boolean isGameOver = isGameOver(gameInput.toCharArray());
-        System.out.println(isGameOver);
+        try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
+            String line;
+            int i = 0;
+            while ((line = reader.readLine()) != null) {
+                i++;
+                String winner = String.valueOf(getWinner(line));
+                log.info("Winner from Line {} ==> {}", i, winner.equals("D") ? "Draw" : winner);
+            }
+        } catch (IOException e) {
 
-
-        gameInput = "XOX=OX=O ";
-        isGameOver = isGameOver(gameInput.toCharArray());
-        System.out.println(isGameOver);
-
-        gameInput = "XOX=OX=O";
-        isGameOver = isGameOver(gameInput.toCharArray());
-        System.out.println(isGameOver);
+        }
     }
 
-    public static boolean isGameOver(char[] inputGameCharacters) {
+    public static boolean isGameOver(String string) {
+        char[] inputGameCharacters = string.toCharArray();
+        if (inputGameCharacters.length > maxRoundPerGame) {
+            log.error("InvalidGame");
+            return false;
+        }
+        for (char gameCharacter : inputGameCharacters) {
+            if (gameCharacter == '=') {
+                return false;
+            }
+        }
+        return true;
+    }
 
-        int i = 0;
-//        if (inputGameCharacters.length == maxRoundPerGame) {
-//            return true;
-//        } else if (inputGameCharacters.length > maxRoundPerGame) {
-//            return false;
-//        } else {
-//            return
-//            //check horizontal
-//            return (inputGameCharacters[i] != ' ' && inputGameCharacters[i] == inputGameCharacters[i + 1] && inputGameCharacters[i] == inputGameCharacters[i + 2])
-//                    //check vertical
-//                    || (inputGameCharacters[i] != ' ' && inputGameCharacters[i] == inputGameCharacters[i + 3] && inputGameCharacters[i] == inputGameCharacters[i + 6])
-//                    //check diagonal
-//                    || (inputGameCharacters[i] != ' ' && inputGameCharacters[i] == inputGameCharacters[i + 4] && inputGameCharacters[i] == inputGameCharacters[i + 8]);
-//
-//        }
+    public static char getWinner(String string) {
+        char[] inputGameCharacters = string.toCharArray();
+        //horizontal
+        for (int i = 0; i < maxRoundPerGame; i += 3) {
+            if (inputGameCharacters[i] != ' ' && inputGameCharacters[i] == inputGameCharacters[i + 1] && inputGameCharacters[i] == inputGameCharacters[i + 2]) {
+                return inputGameCharacters[i];
+            }
+        }
+
+        //vertical
+        for (int i = 0; i < 3; i++) {
+            if (inputGameCharacters[i] != ' ' && inputGameCharacters[i] == inputGameCharacters[i + 3] && inputGameCharacters[i] == inputGameCharacters[i + 6]) {
+                return inputGameCharacters[i];
+            }
+        }
+
+        //diagonal
+        if (inputGameCharacters[0] != ' ' && inputGameCharacters[0] == inputGameCharacters[4] && inputGameCharacters[0] == inputGameCharacters[8]) {
+            return inputGameCharacters[0];
+        }
+        if (inputGameCharacters[2] != ' ' && inputGameCharacters[2] == inputGameCharacters[4] && inputGameCharacters[2] == inputGameCharacters[6]) {
+            return inputGameCharacters[2];
+        }
+
+        return 'D';
 
     }
 
-    public static String getWinner() {
-        return null;
-    }
 }
---
+++ src/test/java/com/interview/TicTacToeTest.java
@@ -0,0 +1,51 @@
+package com.interview;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Author: MohamedSiraj
+ * Date: 9/6/2024
+ * Time: 5:42 PM
+ */
+@Slf4j
+public class TicTacToeTest {
+
+    @Test
+    public void testGameOver() {
+        String string = "XXXOOXXOX";
+        Assertions.assertTrue(TicTacToe.isGameOver(string));
+    }
+
+    @Test
+    public void testGameNotOver() {
+        String string = "XXXOOXXO=";
+        Assertions.assertFalse(TicTacToe.isGameOver(string));
+    }
+
+    @Test
+    public void testInvalidGame() {
+        String string = "XXX=OX=O==";
+        Assertions.assertFalse(TicTacToe.isGameOver(string));
+    }
+
+    @Test
+    public void getHorizontalWinner() {
+        String string = "XXXOOXOOX";
+        Assertions.assertEquals('X', TicTacToe.getWinner(string));
+    }
+
+    @Test
+    public void getVerticalWinner() {
+        String string = "XOX=OX=O=";
+        Assertions.assertEquals('O', TicTacToe.getWinner(string));
+    }
+
+    @Test
+    public void getDiagonalWinner() {
+        String string = "XXO=XO=OX";
+        Assertions.assertEquals('X', TicTacToe.getWinner(string));
+    }
+
+}
