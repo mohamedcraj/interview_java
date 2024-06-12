dc6489d446e3fe96269dec27653ab7d1da161554
mohamedcraj <mohamedcraj@gmail.com>
2024-06-12 15:38:57 +0800

    clean code. deleted unused imports

--- src/main/java/com/interview/TicTacToe.java
+++ src/main/java/com/interview/TicTacToe.java
@@ -5,9 +5,6 @@ import lombok.extern.slf4j.Slf4j;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Stream;
 
 @Slf4j
 public class TicTacToe {
